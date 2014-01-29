5c7df7708b96f52ac77fa6ba412f5ecf06445fad
Jefferson Lam <jlam520@gmail.com>
2014-01-28 18:26:38 -0800

    created new copy of project

---
+++ indexN/README.md
@@ -0,0 +1,30 @@
+StreetViewGallery
+=================
+By: Jefferson Lam
+Last Updated: January 24nd, 2014
+
+Special Thanks To:
+    Google
+    CodingDojo : http://codingdojo.com/
+    Brian Folts : http://www.brianfolts.com/driver/
+    The Folks behind HyperLapse : http://hyperlapse.tllabs.io/
+
+
+//Tasks
+//=====
+
+//DONE: Add scrolling functionality for viewing images
+//DONE: Remove current pointer upon searching again
+//DONE: Functional progress bar
+
+//FRONT-END:
+    //TODO: Make it possible to click and slide progress bar to move between images
+    //TODO: Show loading screen before displaying streetviews and reading input
+
+//BACK-END:
+    //TODO: Remove duplicate street views before calculating headings
+    //TODO: Segment entire route into equidistant segments, and grab an image
+    //      from each segment, rather than grabbing all images at the start
+    //      of the route.
+    //TODO: Set first pointer upon load
+    //TODO: Clean up code. OOP-ify stuff. Less free-floating functions that kinda grab everything from everywhere.
---
+++ indexN/imagehandler.js
@@ -0,0 +1,84 @@
+//imagehandler.js
+
+// BEGIN CLASS DEFINITION
+//======================
+// Class: ImageHandler
+//=================
+// Generates and displays HTML for an array of StreetViewPoints
+// ----------
+// REQUIRED PARAMETERS
+//      divID: String of HTML id where the images will be displayed
+// ----------
+// USAGE
+//      var imageHandler = new imageHandler('streetview-images');
+//      imageHandler.displayHTML(svpArray);
+// ----------
+
+//NOT IMPLEMENTED
+function imageHandler(divID){
+    this.divID = divID;
+
+    this.displayImages = function(svpArray){
+        clearImages();
+        var imgArray = buildImgTags(svpArray);
+        loadImages(imgArray);
+    }
+
+    function clearImages(){
+        document.getElementById(divID).innerHTML = "";
+    }
+
+    function buildImgTags(svpArray){
+        var imgArray = [];
+        for (var i = 0; i<svpArray.length; i++){
+            var img = '<img class="streetview" src="'+svpArray[i].src+'"/>';
+            imgArray[i] = img;
+        }
+        return imgArray;
+    }
+    function loadImages(imgArray){
+        hideImages();
+        
+        //put the img tags onto the page
+        for (var i = 0; i < imgArray.length; i++){
+            document.getElementById(divID).innerHTML += imgArray[i];
+        }
+
+        var imgs = $("#"+divID+" > img").not(function() { return this.complete; });
+        var count = imgs.length;
+
+        //Check if any images need to be loaded
+        if (count) {
+            //Wait for them to load
+            console.log('Loading...');
+            imgs.load(function() {
+                count--;
+                if (!count) {
+                    console.log('Done loading!');
+                    hideLoadingScreen();
+                    showImages();
+                }
+            });
+        //If count is 0 to begin with, all images were already done loading
+        } else {
+            console.log('No loading needed');
+            showImages();
+        }
+    }
+
+    function displayLoadingScreen(){
+        $("#loading").css('visibility', 'visible');
+    }
+
+    function hideLoadingScreen(){
+        $("#loading").css('visibility', 'hidden');
+    }
+
+    function showImages(){
+        $("#"+divID).css('visibility', 'visible');
+    }
+
+    function hideImages(){
+        $("#"+divID).css('visibility', 'hidden');
+    }   
+}
